4b14a6852036192d33f34563049268048ed4ded6
psyko944 <mehdi.kherboucheidf@gmail.com>
2025-10-12 14:25:18 +0200

    use named tuple

--- parser.py
+++ parser.py
@@ -1,29 +1,37 @@
-import re 
+import re
+from collections import namedtuple
 
 charset = "0123456789+-*=/^xX."
 sign = r"([+-]?)"
 number = r"(\d+(\.\d+)?)"
-exponent = r"[xX](\^[0-3])?"
+exponent = r"[xX](\^[0-2])?"
 monomial = rf"{sign}(?:{number}\*{exponent}|{number}|{exponent})"
 polynomial = rf"({monomial})+"
 
 
+Monomial = namedtuple('Monomial', ['coefficient', 'exponent'])
+
 def print_monomial(m):
 	var = '' if m[4] else 'X'
 	
-	s = m[0] if m[0] else '+' #sign
+	s = m[0] if m[0] else '+'
 	e = m[3][1:] if m[3] else m[6][1:] if m[6] else '1' if not m[4] else '0'
-	f = m[2] if m[2] else m[5] if m[5] else '0' # fractional part
-	n = m[4] if m[4] else m[1] if m[1] else '1' # number with var
+	f = m[2] if m[2] else m[5] if m[5] else ''
+	n = m[4] if m[4] else m[1] if m[1] else '1'
+	n = float(n) if f else int(n)
 	print(f"sign: '{s}', number: '{n}', fractional part: '{f}', exponent: '{e}', variable: '{var}'")
+	return Monomial(-n if s == '-' else n, int(e))
+
+# def format_polynomial(lhs, rhs):
 
 def parse_polynomial(equation):
 	if re.fullmatch(polynomial, equation):
 		print("match")
 	p = re.findall(monomial, equation)
 	return p
-	# return [create_monomial(*t) for t in p]
 
+
+#create a function which store all value by same degree then reduice
 def parse(equation):
 	s = equation.replace(" ", "")
 	for x in s:
@@ -39,5 +47,14 @@ def parse(equation):
 	print("LHS terms:", lhs)
 	print("RHS terms:", rhs)
 	print("monomials LHS:")
+	monomials_left = []
+	monomials_right = []
 	for m in lhs:
-		print_monomial(m)
+		monomials_left.append(print_monomial(m))
+	for m in rhs:
+		monomials_right.append(print_monomial(m))
+	print("monomials RHS:")
+	for term in monomials_left:
+		print(term, term.coefficient)
+	for term in monomials_right:
+		print(term)
